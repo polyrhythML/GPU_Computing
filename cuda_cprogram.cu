145ff4facd8cb132ac41528c219dabaa24dbf65f
Amit Singh Bhatti <amit.bhatti@inspirit-iot.com>
2020-04-18 16:06:32 +0530

    added first cuda-c based kernel

---
+++ cuda_cprogram.cu
@@ -0,0 +1,168 @@
+/*
+* This is the cuda C program for the dot product of two matrices.
+We will compile this cuda script using the NVCC compiler and save it as .cu file
+
+* CUDA-C program and library source code filenames always use the ​ .cu ​ file
+extension.
+
+
+*/
+
+// File that has declaration types of all the particular CUDA datatypes.
+#include<cuda_runtime.h>
+
+// For standard IO functions
+#include<stdio.h>
+
+// For using the malloc and free dynamic memory allocation
+#include<stdlib.h>
+
+// create our own version of the np.allclose
+// EPSILON is the delta value to consider two values similar
+#define _EPSILON 0.001
+#define _ABS(x)(x>0.0f ? x:-x)
+
+/*
+We can now create our own version of ​ allclose​ . This will take in two float
+pointers and an integer value, ​ len​ . We loop through both arrays and check
+them: if any points differ by more than _ ​ EPSILON​ , we return -1, otherwise we
+return 0 to indicate that the two arrays do indeed match.
+*/
+
+__host__ int allclose(float *A, float *B, int len)
+{
+
+	int returnval = 0;
+
+	for (int i = 0; i < len; i++)
+	{
+		if ( _ABS(A[i] - B[i]) > _EPSILON )
+		{
+			returnval = -1;
+			break;
+		}
+	}
+
+	return(returnval);
+}
+
+
+// row-column dot-product for matrix multiplication
+__device__ float rowcol_dot(float *matrix_a, float *matrix_b, int row, int col, int N)
+{
+	float val = 0;
+
+	for (int k=0; k < N; k++)
+	{
+        val += matrix_a[ row*N + k ] * matrix_b[ col + k*N];
+	}
+
+	return(val);
+}
+
+// matrix multiplication kernel that is parallelized over row/column tuples.
+__global__ void matrix_mult_ker(float * matrix_a, float * matrix_b, float * output_matrix, int N)
+{
+
+    int row = blockIdx.x*blockDim.x + threadIdx.x;
+    int col = blockIdx.y*blockDim.y + threadIdx.y;
+
+	output_matrix[col + row*N] = rowcol_dot(matrix_a, matrix_b, row, col, N);
+}
+
+
+__host__ int main()
+{
+
+	// Initialize to use first GPU.
+	cudaSetDevice(0);
+
+	// this indicates the width/height of the matrices
+	int N = 4;
+
+	// this will indicate how many bytes to allocate to store a test or output matrix
+	int num_bytes = sizeof(float)*N*N;
+
+	// input test matrix A
+	float h_A[] = {	1.0,  2.0,  3.0,  4.0, \
+					1.0,  2.0,  3.0,  4.0, \
+					1.0,  2.0,  3.0,  4.0, \
+					1.0,  2.0,  3.0,  4.0 };
+
+	// input test matrix B
+	float h_B[] = {	14.0,  13.0,  12.0,  11.0, \
+					14.0,  13.0,  12.0,  11.0, \
+					14.0,  13.0,  12.0,  11.0, \
+					14.0,  13.0,  12.0,  11.0 };
+
+	// expected output of A times B
+	float h_AxB[] = { 140.0,  130.0,  120.0,  110.0, \
+					140.0,  130.0,  120.0,  110.0, \
+					140.0,  130.0,  120.0,  110.0, \
+					140.0,  130.0,  120.0,  110.0 };
+
+
+	// these pointers will be used for the GPU.
+	// (notice how we use normal float pointers)
+	float * d_A;
+	float * d_B;
+	float * d_output;
+
+	// allocate memory for the test matrices on the GPU
+	cudaMalloc((float **) &d_A, num_bytes);
+	cudaMalloc((float **) &d_B, num_bytes);
+
+	// copy the test matrices to the GPU
+	cudaMemcpy(d_A, h_A, num_bytes, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_B, h_B, num_bytes, cudaMemcpyHostToDevice);
+
+	// allocate memory for output on GPU
+	cudaMalloc((float **) &d_output, num_bytes);
+
+	// this will store the output from the GPU
+	float * h_output;
+	h_output = (float *) malloc(num_bytes);
+
+	// setup our block and grid launch parameters with the dim3 class.
+	dim3 block(2,2,1);
+	dim3 grid(2,2,1);
+
+	// launch our kernel
+	matrix_mult_ker <<< grid, block >>> (d_A, d_B, d_output, N);
+
+	// synchronize on the host, to ensure our kernel has finished executing.
+	cudaDeviceSynchronize();
+
+	// copy output from device to host.
+	cudaMemcpy(h_output, d_output, num_bytes, cudaMemcpyDeviceToHost);
+
+	// synchronize again.
+	cudaDeviceSynchronize();
+
+	// free arrays on device.
+	cudaFree(d_A);
+	cudaFree(d_B);
+	cudaFree(d_output);
+
+	// reset the GPU.
+	cudaDeviceReset();
+
+
+	// Check to see if we got the expected output.
+	// in both cases, remember to de-allocate h_output before returning.
+
+	if (allclose(h_AxB, h_output, N*N) < 0)
+	{
+		printf("Error!  Output of kernel does not match expected output.\n");
+		free(h_output);
+		return(-1);
+	}
+	else
+	{
+		printf("Success!  Output of kernel matches expected output.\n");
+		free(h_output);
+		return(0);
+	}
+
+
+}
